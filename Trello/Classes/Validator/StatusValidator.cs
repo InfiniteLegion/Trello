2516417b3313fc69dcef0b7abb6a634ecffbb3a9
InfiniteLegion <dima.kavun.2000@gmail.com>
2024-04-30 17:21:47 +0300

    Проект станом на 27.04.2024

---
+++ Trello/Classes/Validator/StatusValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace Trello.Classes
+{
+    public class StatusValidator
+    {
+        public static void CheckStatusUpdate(Status statusToUpdate, Status originalStatus)
+        {
+            if (statusToUpdate.Name != null)
+            {
+                originalStatus.Name = statusToUpdate.Name;
+            }
+        }
+    }
+}
